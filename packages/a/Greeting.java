b8aed7d9e8187cedcbdf43040bce05d0c4254576
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-01-08 14:18:40 +0530

    packages

--- packages/a/Greeting.java
+++ packages/a/Greeting.java
@@ -1,6 +1,11 @@
 package packages.a;
 
 public class Greeting {
+
+  public static void message() {
+    System.out.println("This is presenr in a");
+  }
+
   public static void main(String[] args) {
     System.out.println("This is Nawaz");
   }
